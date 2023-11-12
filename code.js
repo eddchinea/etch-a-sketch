9b85314eaa0dc55ca0d4b9b15b8f28d0d3aa8ab7
Eduardo <144747945+eddchinea@users.noreply.github.com>
2023-11-12 11:31:37 -0500

    feat: create a 16x16 grid inside the container

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.grid-container');
+
+for (let i = 0; i < 16; i++) {
+    const gridRow = document.createElement('div');
+
+    gridRow.classList.add('grid-row');
+
+    for (let i = 0; i < 16; i++) {
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('grid-column');
+        gridRow.appendChild(gridColumn);
+
+        gridColumn.addEventListener('mouseover', () => {
+            gridColumn.style.backgroundColor = '#000'
+        });
+    }
+
+    container.appendChild(gridRow);
+}
+
