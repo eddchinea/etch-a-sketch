af045ff4abae29b3981851df4eca94c5031556be
Eduardo <144747945+eddchinea@users.noreply.github.com>
2023-11-12 22:28:13 -0500

    feat: write grid logic into a function createGrid()

--- code.js
+++ code.js
@@ -1,20 +1,26 @@
 const container = document.querySelector('.grid-container');
+const btn = document.querySelector('.reset-btn')
 
-for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement('div');
+let size = 16;
 
-    gridRow.classList.add('grid-row');
-
-    for (let i = 0; i < 16; i++) {
-        const gridColumn = document.createElement('div');
-        gridColumn.classList.add('grid-column');
-        gridRow.appendChild(gridColumn);
-
-        gridColumn.addEventListener('mouseover', () => {
-            gridColumn.style.backgroundColor = '#000'
-        });
+function createGrid() {
+    for (let i = 0; i < size; i++) {
+        const gridRow = document.createElement('div');
+    
+        gridRow.classList.add('grid-row');
+    
+        for (let i = 0; i < size; i++) {
+            const gridColumn = document.createElement('div');
+            gridColumn.classList.add('grid-column');
+            gridRow.appendChild(gridColumn);
+    
+            gridColumn.addEventListener('mouseover', () => {
+                gridColumn.style.backgroundColor = '#000';
+            });
+        }
+    
+        container.appendChild(gridRow);
     }
-
-    container.appendChild(gridRow);
 }
 
+createGrid()
